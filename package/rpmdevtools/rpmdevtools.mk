06320c6f289bd39b35d3ca61181f5ffba4f7b67f
Iván Chavero <ichavero@chavero.com.mx>
2025-07-15 11:55:27 -0600

    package/rpmdevtools Add rpmdevtools and help2man packages

---
+++ package/rpmdevtools/rpmdevtools.mk
@@ -0,0 +1,36 @@
+################################################################################
+#
+# rpmdevtools
+#
+################################################################################
+
+RPMDEVTOOLS_VERSION = 9.6
+RPMDEVTOOLS_SOURCE = rpmdevtools-RPMDEVTOOLS_9_6.tar.gz
+RPMDEVTOOLS_SITE = https://pagure.io/rpmdevtools/archive/RPMDEVTOOLS_9_6
+RPMDEVTOOLS_INSTALL_STAGING = YES
+RPMDEVTOOLS_INSTALL_TARGET = YES
+RPMDEVTOOLS_DEPENDENCIES = host-automake host-autoconf help2man python3
+RPMDEVTOOLS_AUTORECONF = YES
+RPMDEVTOOLS_AUTORECONF_OPTS = --add-missing
+ACLOCAL_AUTOMAKE_DIR=$(@D)
+
+define RPMDEVTOOLS_CONFIGURE_CMDS
+	$(TARGET_MAKE_ENV) \
+	cd $(@D)   &&      \
+	aclocal    &&      \
+	autoupdate &&      \
+	autoconf   &&      \
+	automake --add-missing && \
+	./configure --prefix=$(TARGET_DIR)
+endef
+
+define RPMDEVTOOLS_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) install
+endef
+
+define RPMDEVTOOLS_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) install
+endef
+
+
+$(eval $(generic-package))
